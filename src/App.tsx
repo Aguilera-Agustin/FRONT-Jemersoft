8384efac894f6fcb69bf98b3fe319aa7d91e3e15
Agustin Aguilera <agustina@epidataconsulting.com>
2022-01-03 16:49:37 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Divider } from './components/Divider/Divider';
 import { Searcher } from './components/Searcher/Searcher';
 import './styles.css'
 import { PokemonCardContainer } from './components/PokemonCard/PokemonCardContainer';
-import { Footer } from './components/Footer/Footer';
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
     <Searcher/>
     <h4 className='suggestion'>Sugerencias</h4>
     <PokemonCardContainer/> 
-    <Footer/>
   </>
   )
 }
--- src/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import './style.css'
-export const Footer = () => {
-    return (
-        <div className='footerContainer'>
-            Esta aplicación fue realizada por Agustín Aguilera para el Engineer Challenge de Jemersoft
-        </div>
-    )
-}
